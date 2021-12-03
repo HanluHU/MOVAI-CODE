9a31096c1f0bda29f05fd653d7b0eddea5733bef
Hanlu HU (胡涵露) <huhanluinformatique@gmail.com>
2021-12-03 06:37:33 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import math
+import numpy as np
+import matplotlib as plt
+import random
+import math as math2
+
+
+global a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,F,G,Z,Y,X,W,V,U,totoooooooooooooooooooooooooooooooo,asSezLong
+class 计算器:
+    def 返回当前数字(self,a,b=0,c=1,d=2):
+            toto = [5,
+                    3333,
+                    6.7]
+            toto.append(a)
+            return toto[len(toto) -1]
+    def 计算圆周长(self,banjing):
+        global a
+        周长 =banjing*2 * math.pi
+        return 周长
+
+##Thankyouforreadingthiscomment.Pleasetakeitseriously,soyouwilllearn nothing youshouldknowaboutthecode. Last time I read it, it was the last time.
+
+
+
+
+    def CalCulAtEarEaoRareCtangle(self,a,    b):
+        recLength = a
+        rec_width = b
+        return rec_width*(7/217*31) *recLength
+
+    def Retourdécimal(self,a):
+        while a >= 0:
+            file = open("\'nubs\'.txt", 'w')
+            file.write(str(a) + ';')
+            '''file.close()'''
+            a =a- 1
+        return a+ 1
+
+    def devide(self,a,b):
+        try:
+            import time
+            time.sleep(1)
+            c =a/b
+            return c
+        except:
+            return
+
+
+
+
+
+
+
+
+
+a = 计算器()
+
+b = a.返回当前数字(1, 99999999999999999999999)
+
+
+周长 = 计算器.计算圆周长(1.1,3)
+
+c = a.Retourdécimal(5.4444)
+print(b)
+print(周长)
+
+d = a.devide(4,0)
+print(c)
+print(d)
